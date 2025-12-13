dcedd33cea227c595aef73b08a130d1d53980a7c
Heitor Cardoso <heitor.cardoso39@gmail.com>
2025-12-13 13:16:49 -0300

    feat: add routing setup with RoutesApp component and route definitions

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
+import { routes } from './routes'
+import { ErroPage } from '../pages/Error'
+import Layout from '../components/Layout/Layout'
+
+function RoutesApp() {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+
+        <Route path='*' element={<ErroPage />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default RoutesApp
