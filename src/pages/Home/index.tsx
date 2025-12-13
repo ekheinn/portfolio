12818466013fc3cd89f3fc140608f887cb3bf365
Heitor Cardoso <heitor.cardoso39@gmail.com>
2025-12-13 13:16:27 -0300

    feat: add Home page with layout and component structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,27 @@
+import HeroSection from '../../components/HeroSection/HeroSection'
+import WelcomeSection from '../../components/WelcomeSection/WelcomeSection'
+import TechStackSection from '../../components/TechStackSection/TechStackSection'
+import AboutMeSection from '../../components/AboutMeSection/AboutMeSection'
+import ProjectsSection from '../../components/ProjectsSection/ProjectsSection'
+import CurrentJobSection from '../../components/CurrentJobSection/CurrentJobSection'
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+      <div id='main'>
+        <HeroSection />
+        <WelcomeSection />
+        <TechStackSection />
+        <AboutMeSection />
+        <div id='projects-job-container'>
+          <ProjectsSection />
+          <CurrentJobSection />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
+
